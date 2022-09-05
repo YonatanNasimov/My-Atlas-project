256b04fb924bc0136934245a0712cdffa04170ab
YonatanNasimov <112868741+YonatanNasimov@users.noreply.github.com>
2022-09-05 20:21:11 +0300

    Add files via upload

--- js_atlas/atlasManger.js
+++ js_atlas/atlasManger.js
@@ -5,37 +5,42 @@ const showLoading = () => {
    document.querySelector("#id_loading").style.display = "block";
    document.querySelector("#id_row").style.display = "none";
    document.querySelector(".main").style.background = "white";
-   
- }
- // יסתיר את הטעינה ויציג את הרשימה אחרי שהבקשה התקבלה
- const hideLoading = () => {
+
+}
+// יסתיר את הטעינה ויציג את הרשימה אחרי שהבקשה התקבלה
+const hideLoading = () => {
    document.querySelector("#id_loading").style.display = "none";
    document.querySelector("#id_row").style.display = "flex";
-   document.querySelector(".main").style.background = "rgba(172, 255, 47, 0.648)";
-   
- }
+   document.querySelector(".main").style.background = "greenyellow";
+
+}
 
 export const doApi = async () => {
    showLoading();
-   let url ="https://restcountries.com/v3.1/all";
+   let url = "https://restcountries.com/v3.1/all";
    let resp = await fetch(url);
    let data = await resp.json();
-   countries_ar.splice(0,data.length,...data);
+   countries_ar.splice(0, data.length, ...data);
    // console.log(data);
    let startCountries_ar = ["Peru", "Israel", "Russia", "Brazil", "Thailand"];
    let filter_ar = data.filter(item => startCountries_ar.includes(item.name.common));
    console.log(filter_ar);
    creatCountries(filter_ar);
- }
+}
 
-export const creatCountries = _ar  => {
+export const creatCountries = _ar => {
    hideLoading();
    console.log(_ar);
-    document.querySelector("#id_row").innerHTML = "";
-   _ar.forEach(item => {
-      let Country = new CountryItem("#id_row", item);
-      Country.shownRender();
-   });
+   document.querySelector("#id_row").innerHTML = "";
+   if (_ar.length != 0) {
+      _ar.forEach(item => {
+         let Country = new CountryItem("#id_row", item);
+         Country.shownRender();
+
+      })
+   } else{
+      document.querySelector("#id_row").innerHTML = "<h3>Country is undefind</h3>";
+   }
 }
 
 export const searchApi = async _searchQuery => {
@@ -43,4 +48,4 @@ export const searchApi = async _searchQuery => {
    let sorted_ar = countries_ar.filter(item => item.name.common.toLowerCase().includes(_searchQuery.toLowerCase()))
    console.log(sorted_ar);
    creatCountries(sorted_ar);
-   }
+}
